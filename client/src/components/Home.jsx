671e6c72c8ffc7903aabf036074e2dbe0d849904
Iqra Ali <iqra.ali@student.moringaschool.com>
2023-11-01 14:27:15 +0300

    updated files

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function Home () {
+  return (
+    <div>
+        <h2>Home page</h2>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Home;
